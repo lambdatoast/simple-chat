0e6689390abec311d96df0e82851aa189fe9021f
Alexander Noriega <anoriega@hexacta.com>
2019-12-14 22:02:32 -0300

    style: SVG icons for header.

--- client/src/app/containers/Header/index.tsx
+++ client/src/app/containers/Header/index.tsx
@@ -5,40 +5,60 @@ import { setActivePage } from "app/actions";
 import { AppState } from "app/reducers";
 import { connect } from "react-redux";
 import { NavigationState, NavigationPage } from "app/models";
+import SettingsIcon from "./icons/settings.svg";
+import ChatIcon from "./icons/chatbubbles.svg";
+import * as classNames from "classnames";
 
 interface HeaderComponentProps {
 	navigation: NavigationState;
 	setActivePage: (p: NavigationPage) => void;
 }
 
+const classes = (root: string, p: NavigationPage, n: NavigationState) =>
+	classNames({
+		[style[`${root}`]]: true,
+		[style[`${root}Active`]]: n.activePage === p,
+		[style[`${root}Default`]]: n.activePage !== p
+	});
+
 function HeaderComponent(props: HeaderComponentProps) {
 	const { setActivePage, navigation } = props;
 	return (
 		<nav className={style.nav}>
-			<ul>
-				<li>
+			<ul className={style.navList}>
+				<li className={style.navItem}>
 					<Link
+						className={classes("navLink", "Chat", navigation)}
 						to="/"
 						onClick={() => {
 							setActivePage("Chat");
 						}}
 					>
-						Chat
+						<ChatIcon
+							className={classes("navLinkIcon", "Chat", navigation)}
+							height="2em"
+						/>
 						{navigation.unreadCount > 0 ? (
 							<span className={style.unreadCount}>
 								{navigation.unreadCount}
 							</span>
 						) : null}
+						<span>Chat</span>
 					</Link>
 				</li>
-				<li>
+				<li className={style.navItem}>
 					<Link
+						className={classes("navLink", "Settings", navigation)}
 						to="/settings"
 						onClick={() => {
 							setActivePage("Settings");
 						}}
 					>
-						Settings
+						<SettingsIcon
+							className={classes("navLinkIcon", "Settings", navigation)}
+							height="2em"
+						/>
+						<span>Settings</span>
 					</Link>
 				</li>
 			</ul>
--- client/types/global.d.ts
+++ client/types/global.d.ts
@@ -11,6 +11,11 @@ declare module "*.scss" {
 	export = styles;
 }
 
+declare module "*.svg" {
+	const svgComponent: React.ComponentType<React.SVGAttributes<{}>>;
+	export default svgComponent;
+}
+
 // Omit type https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046
 // type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 type PartialPick<T, K extends keyof T> = Partial<T> & Pick<T, K>;
